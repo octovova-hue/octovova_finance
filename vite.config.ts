ab3d5d7503746f4267d6dccef2159845beca1cc7
octovova-hue <octovova@gmail.com>
2026-09-03 02:59:22 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
   server: {
     port: 3000,
     open: false
+  },
+
+  preview: {
+    host: '0.0.0.0',
+    allowedHosts: ['octovova-finance.onrender.com']
   }
 });
